a29524bec0b372401c9d611ee09f5a0741ec1bcc
Jim Strother <jim.strother@gmail.com>
2016-06-08 19:12:21 -0700

    almost ready to get individual emails rendered properly

--- index.jsx
+++ index.jsx
@@ -5,40 +5,40 @@ const Router = router.Router;
 const Route = router.Route;
 const hashHistory = router.hashHistory;
 
-var EMAILS = {
-	inbox: {
-		0: {
-			id: 0,
-			from: "billg@microsoft.com",
-			to: "TeamWoz@Woz.org",
-			title: "Possible work opportunity",
-			content: "Dear Woz.  Fancy a job at Mister Softee? Bill x"
-		},
-		1: {
-			id: 1,
-			from: "zuck@facebook.com",
-			to: "TeamWoz@Woz.org",
-			title: "Do you know PHP?",
-			content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
-		}
+var EMAILS = [
+	{
+		id: 0,
+		from: "billg@microsoft.com",
+		to: "TeamWoz@Woz.org",
+		title: "Possible work opportunity",
+		content: "Dear Woz.  Fancy a job at Mister Softee? Bill x",
+		mailbox: 'inbox'
 	},
-	spam: {
-		0: {
-			id: 0,
-			from: "ChEaPFlighTZ@hotmail.com",
-			to: "TeamWoz@Woz.org",
-			title: "WaNT CHEEp FlitZ",
-			content: " Theyre CheEp"
-		},
-		1: {
-			id: 1,
-			from: "NiKEAIRJordanZ@hotmail.com",
-			to: "TeamWoz@Woz.org",
-			title: "JorDanz For SAle",
-			content: "Theyre REELY CheEp"
-		}
+	{
+		id: 1,
+		from: "zuck@facebook.com",
+		to: "TeamWoz@Woz.org",
+		title: "Do you know PHP?",
+		content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x",
+		mailbox: 'inbox'
+	},
+	{
+		id: 2,
+		from: "ChEaPFlighTZ@hotmail.com",
+		to: "TeamWoz@Woz.org",
+		title: "WaNT CHEEp FlitZ",
+		content: " Theyre CheEp",
+		mailbox: 'spam'
+	},
+	{
+		id: 3,
+		from: "NiKEAIRJordanZ@hotmail.com",
+		to: "TeamWoz@Woz.org",
+		title: "JorDanz For SAle",
+		content: "Theyre REELY CheEp",
+		mailbox: 'spam'
 	}
-};
+];
 
 var Email = function(props) {
 	return (
@@ -49,81 +49,58 @@ var Email = function(props) {
 			<br />
 			{props.title}
 			<br />
-			{props.content}
-			<br />
 		</div>
 	);
 };
 
-var Inbox = function(props) {
-	var emails = Object.keys(props.emails.inbox).map(function(emailID, index) {
-		var email = props.emails.inbox[emailID];
-		return (
-			<li key={index}>
-				<Email id={email.id} from={email.from} to={email.to} title={email.title} content={email.content} />
-			</li>
+var EmailList = React.createClass({
+	render: function() {
+		var route = this.props.route.path;
+		var title = route.replace(/\b\w*/g, function(text) {
+				return (text.charAt(0).toUpperCase() + text.substr(1).toLowerCase());
+			}
 		);
-	});
-
-	return (
-		<div>
-			<h1>Inbox</h1>
-			<ul>
-				{emails}
-			</ul>
-		</div>
-	);
-};
-
-var Spam = function(props) {
-	var emails = Object.keys(props.emails.spam).map(function(emailID, index) {
-		var email = props.emails.spam[emailID];
+		var emails = EMAILS.filter(email => (email.mailbox === route)).map(function(email, index) {
+			return (
+				<li key={index}>
+					<Email id={email.id} from={email.from} to={email.to} title={email.title} content={email.content} />
+	 			</li>
+			)
+		});
 		return (
-			<li key={index}>
-				<Email id={email.id} from={email.from} to={email.to} title={email.title} content={email.content} />
-			</li>
-		);
-	});
+			<div>
+				<h1>{title}</h1>
+				<ul>
+					{emails}
+				</ul>
+			</div>
+		)
+	}
+});
 
+var EmailContainer = function(props) {
 	return (
 		<div>
-			<h1>Spam</h1>
-			<ul>
-				{emails}
-			</ul>
-		</div>
-	);
-};
-
-var InboxContainer = function() {
-	return (
-		<div>
-			<Inbox emails={EMAILS} />
-		</div>
-	);
-};
-
-var SpamContainer = function() {
-	return (
-		<div>
-			<Spam emails={EMAILS} />
-		</div>
-	);
-};
-
-var EmailContainer = function() {
-	return (
-		<div>
-			
+			<aside>
+				<ul>
+					<li>Inbox</li>
+					<li>Spam</li>
+				</ul>
+			</aside>
+		
+			<main>
+				{props.children}
+			</main>
 		</div>
 	);
 };
 
 var routes = (
 	<Router history={hashHistory}>
-		<Route path="/email" component={EmailContainer} />
-		<Route path="/inbox" component={InboxContainer} />
-		<Route path="/spam" component={SpamContainer} />
+		<Route path="/" component={EmailContainer}>
+			<Route path="inbox" component={EmailList} />
+			<Route path="spam" component={EmailList} />
+		</Route>
 	</Router>
 );
 
